a5ee26d0db12f9ea604790d97715a1037012cce2
Abanoub <pophtc.aa@gmail.com>
2022-04-06 13:28:06 +0200

    fixing the scalability in changing the hight only, and fixing the answers overriding

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 //web Scalable 
 function doResize() {
-    let container = document.getElementById("page_wrapper");
+    console.log("scaled")
     let content = document.getElementById("page_content")
 
-    let contentWidth = content.clientWidth;
-    
-    if(contentWidth > container.offsetWidth){
-        let scale = container.clientWidth/contentWidth;
-        content.style = `transform: translateX(-50%) 
-        translateY(-50%) scale(${scale}) ;`;
-    }else{
-        content.style =`transform: translateX(-50%)
-         translateY(-50%)`
-    }
+    var currentWidth = content.clientWidth;
+    var currentHeight = content.clientHeight;
 
+    var availableHeight = window.innerHeight;
+    var availableWidth = window.innerWidth;
+
+    var scaleX = availableWidth / currentWidth;
+    var scaleY = availableHeight / currentHeight;
+    console.log(currentWidth,currentHeight , availableWidth, availableHeight)
+    scaleX = Math.min( scaleX, scaleY);
+    scaleY = scaleX;
+
+    var translationX = Math.round((availableWidth - (currentWidth * scaleX)) / 2);
+  var styles = {
     
+    "-webkit-transform": "translate(" + translationX + "px) scale3d("
+                                      + scaleX + ", " + scaleY + ", 1)",
+    "-webkit-transform-origin": "0 0"
+    };
+    
+    Object.assign(content.style, styles)
+
 }
 window.onresize =doResize;
 
@@ -62,28 +72,29 @@ const optionClicked = (e)=>{
     rightAns =e.target.className.split(" ")[1]
     
 }
-// const correctAudio = new Audio();
-// const wrongAudio = new Audio()
+
 const answerClicked =(e)=>{
     if(answer){
         e.target.innerHTML = `<p>${answerPicked}</p>`;
         console.log(rightAns === "correct")
         if(rightAns === "correct"){
             setTimeout(()=>{e.target.innerHTML += '<img class="true" src="./assets/images/tikMark-small.png">'},1000)
-            // correctAudio.play()
             answer.style ="visibility:hidden"
             answerPicked = " ";
             answer="";
+            e.target.removeEventListener('click', answerClicked)
         }else{
             setTimeout(()=>{e.target.innerHTML += '<img class="false" src="./assets/images/crossMark-small.png">'},500)
             setTimeout(()=>{e.target.innerHTML = " "},1500)
-            // wrongAudio.play()
         }
 
     }
 }
 // Replay the task
-
+let ansSpaces = Array.from(document.getElementsByClassName("answer"))
+    ansSpaces.forEach(el=>{
+        el.addEventListener('click',answerClicked)
+    })
 const reset =()=>{
     let options = Array.from(document.getElementsByClassName("option"))
     options.forEach(el=>{
